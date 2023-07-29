061ff0525196b4e9bd3b39129bb9056067af633e
Ulvi <102907551+oolyvi@users.noreply.github.com>
2023-07-29 08:24:00 +0400

    Update UiEvent.kt

--- app/src/main/java/com/urahimli/todolist/util/UiEvent.kt
+++ app/src/main/java/com/urahimli/todolist/util/UiEvent.kt
@@ -1,8 +1,6 @@
 package com.urahimli.todolist.util
 
-//one time eventler ucun istifade edeceyik
 sealed class UiEvent {
-
     object PopBackStack: UiEvent()
     data class Navigate(val route: String): UiEvent()
     data class ShowSnackbar(
